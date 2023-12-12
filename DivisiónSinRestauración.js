52bd8ea9b88f688e30cfee34179bad5a9e58b049
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-12-12 09:22:30 -0500

    first commit

---
+++ DivisiónSinRestauración.js
@@ -0,0 +1,114 @@
+function sum(num1, num2) {
+    const maxLength = Math.max(num1.length, num2.length);
+  
+    if (num1.length !== num2.length) {
+      if (num1.length < num2.length) {
+        while (num1.length < num2.length) {
+          num1 = "0" + num1;
+        }
+      } else if (num1.length > num2.length) {
+        while (num1.length > num2.length) {
+          num2 = "0" + num2;
+        }
+      }
+    }
+  
+    let sum = "";
+    let carry = 0;
+  
+    for (let i = maxLength - 1; i >= 0; i--) {
+      const bit1 = parseInt(num1[i]);
+      const bit2 = parseInt(num2[i]);
+  
+      const total = bit1 + bit2 + carry;
+  
+      const resultBit = total % 2;
+      carry = total >= 2 ? 1 : 0;
+  
+      sum = resultBit.toString() + sum;
+    }
+  
+    return sum;
+  }
+  
+  function subtract(num1, num2) {
+    const maxLength = Math.max(num1.length, num2.length);
+  
+    if (num1.length !== num2.length) {
+      if (num1.length < num2.length) {
+        while (num1.length < num2.length) {
+          num1 = "0" + num1;
+        }
+      } else if (num1.length > num2.length) {
+        while (num1.length > num2.length) {
+          num2 = "0" + num2;
+        }
+      }
+    }
+  
+    let result = "";
+    let borrow = 0;
+  
+    for (let i = maxLength - 1; i >= 0; i--) {
+      const bit1 = parseInt(num1[i]);
+      const bit2 = parseInt(num2[i]);
+  
+      let total = bit1 - bit2 - borrow;
+  
+      if (total < 0) {
+        total += 2;
+        borrow = 1;
+      } else {
+        borrow = 0;
+      }
+  
+      result = total.toString() + result;
+    }
+  
+    return result;
+  }
+
+function divisionSinRestauracion(dividendo, divisor) {
+    let R = "";
+    let D = dividendo.slice(0, divisor.length);
+    let C = "";
+    let cont = divisor.length;
+  
+    R = subtract(D, divisor);
+  
+    while (cont < dividendo.length) {
+      if (R[0] == "0") {
+        C += "1";
+        R = R + dividendo[cont];
+        R = subtract(R, divisor);
+        R = R.substring(1);
+      } else {
+        C += "0";
+        R = R + dividendo[cont];
+        R = sum(R, divisor);
+        R = R.substring(1);
+      }
+      cont++;
+    }
+  
+    if (R[0] == "0") {
+      C += "1";
+    } else {
+      C += "0";
+      R = sum(R, divisor);
+      R = R.substring(1);
+    }
+  
+    return { cociente: C, residuo: R };
+  }
+
+  // Ejemplo de uso
+const dividendo = "1011100"; // Por ejemplo, 92 en binario
+const divisor = "101"; // Por ejemplo, 5 en binario
+// const dividendo = "1000";
+// const divisor = "10";
+
+const { cociente, residuo } = divisionSinRestauracion(dividendo, divisor);
+
+console.log("Cociente:", cociente); // Imprime el cociente
+console.log("Residuo:", residuo); // Imprime el residuo
